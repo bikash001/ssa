e6fb0677d2981ea360f19943f1a7c98923e382ad
Bikash Gogoi <bikashgogoi001@gmail.com>
2017-04-09 23:12:43 +0530

    modified while condn

--- utility.js
+++ utility.js
@@ -1,24 +1,31 @@
-function changeId(temp, globalKeys, keys) {
+function changeId(temp, globalKeys, keys, definedKey) {
     // if (test) {
     //     console.log('hello');
     //     console.log(globalKeys);
     // }
     if (temp.length > 2) {
         if (temp[1].val == '=') {
-            globalKeys[temp[0].val] += 1;
-            keys[temp[0].val] = "";
-            temp[0].val += globalKeys[temp[0].val];
             for (var x=2; x<temp.length; x++) {
                 if (temp[x].type == 'id') {
                     // console.log(stmts[i].val[x].val, 'test');
-                    globalKeys[temp[x].val] += 1;
-                    temp[x].val += globalKeys[temp[x].val];
+                    if (definedKey.hasOwnProperty(temp[x].val)) {
+                        temp[x].val += definedKey[temp[x].val];
+                    } else {
+                        temp[x].val += keys[temp[x].val]
+                    }
                 }
             }
+            globalKeys[temp[0].val] += 1;
+            definedKey[temp[0].val] = globalKeys[temp[0].val];
+            temp[0].val += globalKeys[temp[0].val];
         } else {
             for (var x=0; x<temp.length; x++) {
                 if (temp[x].type == 'id') {
-                    temp[x].val += globalKeys[temp[x].val];
+                    if (definedKey.hasOwnProperty(temp[x].val)) {
+                        temp[x].val += definedKey[temp[x].val];
+                    } else {
+                        temp[x].val += keys[temp[x].val]
+                    }
                 }
             }
         }
@@ -26,10 +33,171 @@ function changeId(temp, globalKeys, keys) {
     
 }
 
-exports.cfg = function cfg(cmpstmt, keys, tempkey, parentJoinNode) {
-    parentJoinNode = parentJoinNode || {};
-    var keyList = tempkey || {};
-    var globalKeys = keys || {};
+function propagateVars(stmt, keys) {
+    var stmts;
+    var temp;
+    var tempkeys = {};
+    if (stmt.type == 'cmpstmt') {
+        stmts = cmpstmt.val;
+        // console.log(stmts);
+    } else {
+        stmts = [stmt];
+    }
+
+    for (var i=0; i<stmts.length; i++) {
+        // console.log(stmts[i]);
+        if (stmts[i].type == 'decstmt') {
+            temp = stmts[i].val;
+            for (var x=0; x<temp.length; x++) {
+                if (temp[x].type == 'id') {
+                    keys[x] = '';
+                }
+            }
+        } else if (stmts[i].type == 'expstmt') {
+            temp = stmts[i].val;
+            if (temp.length > 2) {
+                if (temp[1].val == '=') {
+                   keys[x] = ""; 
+                }
+            }
+        } else if (stmts[i].type == 'ifstmt') {
+
+            temp = stmts[i].val;
+            type = temp.type
+            if ( type == 'expstmt' || type == 'decstmt') {  
+                propagateVars(temp,tempKeys);
+            } else if (type == 'cmpstmt') {
+                
+            } else if (type == 'ifstmt' || type == 'whilestmt'){
+                
+            }
+            if (Object.keys(stmts[i].val.else).length > 0) {
+                type = stmts[i].val.else.type;
+                if (type == 'expstmt' || type == 'decstmt' || type == 'jmpstmt') {
+                    var tempblock = BasicBlock();
+                    tempblock.ins.push(stmts[i].val.else);
+                    entryblock.succ.push(tempblock);
+                    exitblock.pred.push(tempblock);
+                    
+                    var tempKeys = getCopy(keyList);
+                    for (var key in definedKey) {
+                        tempKeys[key] = definedKey[key];
+                    }
+                    changeId(temp.if.val, globalKeys, tempKeys, elkeyList);
+                } else {
+                    var tempKeys = getCopy(keyList);
+                    for (var key in definedKey) {
+                        tempKeys[key] = definedKey[key];
+                    }
+                    var ret = cfg(stmts[i].val.else, globalKeys, tempKeys, elkeyList);
+                    ret.exit.succ.push(exitblock);
+                    ret.entry.pred.push(entryblock);
+                    entryblock.succ.push(ret.entry);
+                    exitblock.pred.push(ret.exit);
+                }
+            } else {
+                exitblock.pred.push(entryblock);
+                entryblock.succ.push(exitblock);
+            }
+
+            for (var x in ifkeyList) {
+                globalKeys[x] += 1;
+                if (elkeyList[x] != undefined) {
+                    // console.log(1,x+globalKeys[x]+' = Phi('+x+ifkeyList[x]+","+x+elkeyList[x]+")");
+                    exitblock.ins.push({type:'exp',val: [new node('',x+globalKeys[x]+' = Phi('+x+ifkeyList[x]+","+x+elkeyList[x]+")")]});
+                } else {
+                    // console.log(2,x+globalKeys[x]+' = Phi('+x+ifkeyList[x]+","+x+keyList[x]+")");
+                    exitblock.ins.push({type:'exp',val: [new node('',x+globalKeys[x]+' = Phi('+x+ifkeyList[x]+","+x+keyList[x]+")")]});
+                }
+            }
+            for (var x in elkeyList) {
+                if (ifkeyList[x] == undefined) {
+                    globalKeys[x] += 1;
+                    // console.log(3,x+globalKeys[x]+' = Phi('+x+keyList[x]+","+x+elkeyList[x]+")");
+                    exitblock.ins.push({type:'exp',val: [new node('',x+globalKeys[x]+' = Phi('+x+elkeyList[x]+","+x+keyList[x]+")")]});
+                }
+            }
+            console.log(test);
+            if (Object.keys(retval.entry).length > 0) {
+                retval.exit.succ.push(entryblock);
+                entryblock.pred.push(retval.exit);
+                retval.exit = exitblock;
+            } else {
+                retval.entry = entryblock;
+                retval.exit = exitblock;
+            }
+            ifkeyList = {};
+            elkeyList = {};
+            exp.join = exitblock;
+            stmts[i].join = exitblock;
+        } else {
+            var entryblock = BasicBlock();
+            var whilekeyList = {};
+            // var exitblock = BasicBlock();
+            if (bb != undefined) {
+                if (Object.keys(retval.entry).length > 0) {
+                    bb.pred.push(retval.exit);
+                    retval.exit.succ.push(bb);
+                    retval.exit = bb;
+                } else {
+                    retval.entry = bb;
+                    retval.exit = bb;
+                }
+                bb = undefined;
+            }
+
+            temp = stmts[i].val;
+            changeId(temp.exp, globalKeys, keyList, definedKey);
+            var exp = new node('while-cond',temp.exp);
+            entryblock.ins.push(exp);
+            var type = temp.body.type;
+
+            if ( type == 'expstmt' || type == 'decstmt' || type == 'jmpstmt') {
+                var tempblock = BasicBlock();
+                tempblock.ins.push(temp.body);
+                entryblock.succ.push(tempblock);
+                entryblock.pred.push(tempblock);
+                var tempKeys = getCopy(keyList);
+                for (var key in definedKey) {
+                    tempKeys[key] = definedKey[key];
+                }
+                changeId(temp.body.val, globalKeys, tempKeys, whilekeyList);
+            } else {
+                var list = {};
+                propagateVars(stmts[i].val.body,list);
+                var ret = cfg(stmts[i].val.body);
+                ret.exit.succ.push(entryblock);
+                ret.entry.pred.push(entryblock);
+                entryblock.succ.push(ret.entry);
+                entryblock.pred.push(ret.exit);
+            }
+            if (Object.keys(retval.entry).length > 0) {
+                retval.exit.succ.push(entryblock);
+                entryblock.pred.push(retval.exit);
+                retval.exit = entryblock;
+            } else {
+                retval.entry = entryblock;
+                retval.exit = entryblock;
+            }
+            exp.join = entryblock;
+            stmts[i].join = entryblock;
+        }
+    }
+}
+
+function getCopy(vals) {
+    var obj = {};
+    for (var x in vals) {
+        obj[x] = vals[x];
+    }
+    return obj;
+}
+
+exports.cfg = function cfg(cmpstmt, globalKeys, keyList, definedKey) {
+    // parentJoinNode = parentJoinNode || {};
+    keyList = keyList || {};
+    globalKeys = globalKeys || {};
+    definedKey = definedKey || {};
 
     var retval = {entry: {}, exit: {}};
     var bb;
@@ -53,7 +221,7 @@ exports.cfg = function cfg(cmpstmt, keys, tempkey, parentJoinNode) {
             temp = stmts[i].val;
             for (var x=0; x<temp.length; x++) {
                 if (temp[x].type == 'id') {
-                    keyList[temp[x].val] = "";
+                    definedKey[temp[x].val] = 0;
                     globalKeys[temp[x].val] = 0;
                     temp[x].val += '0';
                 }
@@ -64,15 +232,14 @@ exports.cfg = function cfg(cmpstmt, keys, tempkey, parentJoinNode) {
             }
             bb.ins.push(stmts[i]);
             temp = stmts[i].val;
-            changeId(temp, globalKeys, keyList);
-            // console.log('hello');
-            // console.log(bb);
+            changeId(temp, globalKeys, keyList, definedKey);
+
         } else if (stmts[i].type == 'jmpstmt') {
             if (bb == undefined) {
                 bb = BasicBlock();
             }
             bb.ins.push(stmts[i]);
-            changeId(stmts[i].val, globalKeys, keyList);
+            changeId(stmts[i].val, globalKeys, keyList, definedKey);
         } else if (stmts[i].type == 'ifstmt') {
             var entryblock = BasicBlock();
             var exitblock = BasicBlock();
@@ -91,27 +258,29 @@ exports.cfg = function cfg(cmpstmt, keys, tempkey, parentJoinNode) {
 
             temp = stmts[i].val;
             // console.log(temp.exp);
-            changeId(temp.exp, globalKeys, keyList);
+            changeId(temp.exp, globalKeys, keyList, definedKey);
             var exp = new node('if-cond',temp.exp);
             entryblock.ins.push(exp);
             var type = temp.if.type;
             var oldKey;
-            var ifkeyList = [];
-            var elkeyList = [];
+            var ifkeyList = {};
+            var elkeyList = {};
             if ( type == 'expstmt' || type == 'decstmt' || type == 'jmpstmt') {
                 var tempblock = BasicBlock();
                 tempblock.ins.push(temp.if);
                 entryblock.succ.push(tempblock);
                 exitblock.pred.push(tempblock);
-                changeId(temp.if.val, globalKeys, ifkeyList);
-                for (var key in ifkeyList) {
-                    keyList[key] = "";
+                var tempKeys = getCopy(keyList);
+                for (var key in definedKey) {
+                    tempKeys[key] = definedKey[key];
                 }
-//here we come
-
+                changeId(temp.if.val, globalKeys, tempKeys, ifkeyList);
             } else{
-                oldKey = Object.create(globalKeys);
-                var ret = cfg(stmts[i].val.if, globalKeys, );
+                var tempKeys = getCopy(keyList);
+                for (var key in definedKey) {
+                    tempKeys[key] = definedKey[key];
+                }
+                var ret = cfg(stmts[i].val.if, globalKeys, tempKeys, ifkeyList);
                 ret.exit.succ.push(exitblock);
                 ret.entry.pred.push(entryblock);
                 entryblock.succ.push(ret.entry);
@@ -124,8 +293,18 @@ exports.cfg = function cfg(cmpstmt, keys, tempkey, parentJoinNode) {
                     tempblock.ins.push(stmts[i].val.else);
                     entryblock.succ.push(tempblock);
                     exitblock.pred.push(tempblock);
+                    
+                    var tempKeys = getCopy(keyList);
+                    for (var key in definedKey) {
+                        tempKeys[key] = definedKey[key];
+                    }
+                    changeId(temp.if.val, globalKeys, tempKeys, elkeyList);
                 } else {
-                    var ret = cfg(stmts[i].val.else);
+                    var tempKeys = getCopy(keyList);
+                    for (var key in definedKey) {
+                        tempKeys[key] = definedKey[key];
+                    }
+                    var ret = cfg(stmts[i].val.else, globalKeys, tempKeys, elkeyList);
                     ret.exit.succ.push(exitblock);
                     ret.entry.pred.push(entryblock);
                     entryblock.succ.push(ret.entry);
@@ -135,6 +314,25 @@ exports.cfg = function cfg(cmpstmt, keys, tempkey, parentJoinNode) {
                 exitblock.pred.push(entryblock);
                 entryblock.succ.push(exitblock);
             }
+
+            for (var x in ifkeyList) {
+                globalKeys[x] += 1;
+                if (elkeyList[x] != undefined) {
+                    // console.log(1,x+globalKeys[x]+' = Phi('+x+ifkeyList[x]+","+x+elkeyList[x]+")");
+                    exitblock.ins.push({type:'exp',val: [new node('',x+globalKeys[x]+' = Phi('+x+ifkeyList[x]+","+x+elkeyList[x]+")")]});
+                } else {
+                    // console.log(2,x+globalKeys[x]+' = Phi('+x+ifkeyList[x]+","+x+keyList[x]+")");
+                    exitblock.ins.push({type:'exp',val: [new node('',x+globalKeys[x]+' = Phi('+x+ifkeyList[x]+","+x+keyList[x]+")")]});
+                }
+            }
+            for (var x in elkeyList) {
+                if (ifkeyList[x] == undefined) {
+                    globalKeys[x] += 1;
+                    // console.log(3,x+globalKeys[x]+' = Phi('+x+keyList[x]+","+x+elkeyList[x]+")");
+                    exitblock.ins.push({type:'exp',val: [new node('',x+globalKeys[x]+' = Phi('+x+elkeyList[x]+","+x+keyList[x]+")")]});
+                }
+            }
+            console.log(test);
             if (Object.keys(retval.entry).length > 0) {
                 retval.exit.succ.push(entryblock);
                 entryblock.pred.push(retval.exit);
@@ -143,10 +341,13 @@ exports.cfg = function cfg(cmpstmt, keys, tempkey, parentJoinNode) {
                 retval.entry = entryblock;
                 retval.exit = exitblock;
             }
+            ifkeyList = {};
+            elkeyList = {};
             exp.join = exitblock;
             stmts[i].join = exitblock;
         } else {
             var entryblock = BasicBlock();
+            var whilekeyList = {};
             // var exitblock = BasicBlock();
             if (bb != undefined) {
                 if (Object.keys(retval.entry).length > 0) {
@@ -159,15 +360,26 @@ exports.cfg = function cfg(cmpstmt, keys, tempkey, parentJoinNode) {
                 }
                 bb = undefined;
             }
-            var exp = new node('while-cond',stmts[i].val.exp);
+
+            temp = stmts[i].val;
+            changeId(temp.exp, globalKeys, keyList, definedKey);
+            var exp = new node('while-cond',temp.exp);
             entryblock.ins.push(exp);
-            var type = stmts[i].val.body.type;
+            var type = temp.body.type;
+
             if ( type == 'expstmt' || type == 'decstmt' || type == 'jmpstmt') {
                 var tempblock = BasicBlock();
-                tempblock.ins.push(stmts[i].val.body);
+                tempblock.ins.push(temp.body);
                 entryblock.succ.push(tempblock);
                 entryblock.pred.push(tempblock);
+                var tempKeys = getCopy(keyList);
+                for (var key in definedKey) {
+                    tempKeys[key] = definedKey[key];
+                }
+                changeId(temp.body.val, globalKeys, tempKeys, whilekeyList);
             } else {
+                var list = {};
+                propagateVars(stmts[i].val.body,list);
                 var ret = cfg(stmts[i].val.body);
                 ret.exit.succ.push(entryblock);
                 ret.entry.pred.push(entryblock);
@@ -183,6 +395,7 @@ exports.cfg = function cfg(cmpstmt, keys, tempkey, parentJoinNode) {
                 retval.exit = entryblock;
             }
             exp.join = entryblock;
+            stmts[i].join = entryblock;
         }
     }
     if (bb != undefined) {
@@ -257,6 +470,7 @@ function printInstruction(arg, sp) {
                 console.log(space+'else');
                 printInstruction(stmts[i].val.else,space);
             }
+            // console.log('hey', stmts[i].join.ins.length);
             for (var x=0; x<stmts[i].join.ins.length; x++) {
                print_single_inst(stmts[i].join.ins[x].val,space); 
             }
